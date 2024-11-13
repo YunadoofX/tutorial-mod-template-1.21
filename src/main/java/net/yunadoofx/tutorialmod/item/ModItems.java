11af26bbd342ceb301ff02889eb7e90fb6355950
A Wise Guru <yuvaisboss@gmail.com>
2024-11-13 17:48:28 -0600

    added a very important comment frfr

---
+++ src/main/java/net/yunadoofx/tutorialmod/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.yunadoofx.tutorialmod.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.yunadoofx.tutorialmod.TutorialMod;
+
+public class ModItems {
+    public static final Item SUPER_AMETHYST = registerItem(name: "super_amethyst", new Item(new Item.Settings()));
+
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+}
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+    }
+
+
+}
