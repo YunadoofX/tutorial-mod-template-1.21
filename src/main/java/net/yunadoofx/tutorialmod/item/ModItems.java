d8e8ccac3ee9eb2f7e941f4aabe93600f70dfdb9
A Wise Guru <yuvaisboss@gmail.com>
2024-11-16 20:11:18 -0600

    added a super amethyst finally

--- src/main/java/net/yunadoofx/tutorialmod/item/ModItems.java
+++ src/main/java/net/yunadoofx/tutorialmod/item/ModItems.java
@@ -1,13 +1,17 @@
 package net.yunadoofx.tutorialmod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.yunadoofx.tutorialmod.TutorialMod;
 
 public class ModItems {
-    public static final Item SUPER_AMETHYST = registerItem(name: "super_amethyst", new Item(new Item.Settings()));
+
+    public static final Item SUPER_AMETHYST = registerItem("super_amethyst", new Item(new Item.Settings()));
+
 
 //6:22
 
@@ -17,6 +21,11 @@ public class ModItems {
 
     public static void registerModItems() {
         TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(SUPER_AMETHYST);
+        });
     }
 
 
